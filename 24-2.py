b00bdd419a5073a666bbb9f6cb2ae7a22baa74a1
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-01 16:38:18 +0300

    update 24-2.py

--- 24-2.py
+++ 24-2.py
@@ -1,17 +1,24 @@
 f = open('24.txt')
 s = f.readline()
-cnt = 1
-max_cnt = 1
-for i in range(1, len(s)):
+cnt = 0
+max_cnt = 0
+while i < len(s):
     if s[i:i+3] == 'XYZ':
         cnt += 3
         max_cnt = max(cnt, max_cnt)
+        i += 3
     elif s[i:i+2] == 'XY':
         cnt += 2
         max_cnt = max(cnt, max_cnt)
+        i += 2
         cnt = 0
     elif s[i:i+1] == 'X':
         cnt += 1
         max_cnt = max(cnt, max_cnt)
+        i += 1
         cnt = 0
+    else:
+        cnt = 0
+        i += 1
+
 print(max_cnt)
