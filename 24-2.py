0906f09a9f1a171655673ac5eeafe3a93059e53e
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-01 16:35:48 +0300

    update 24-2.py

--- 24-2.py
+++ 24-2.py
@@ -6,6 +6,12 @@ for i in range(1, len(s)):
     if s[i:i+3] == 'XYZ':
         cnt += 3
         max_cnt = max(cnt, max_cnt)
-    else:
-        cnt = 1
+    elif s[i:i+2] == 'XY':
+        cnt += 2
+        max_cnt = max(cnt, max_cnt)
+        cnt = 0
+    elif s[i:i+1] == 'X':
+        cnt += 1
+        max_cnt = max(cnt, max_cnt)
+        cnt = 0
 print(max_cnt)
