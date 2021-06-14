d19056626f958bb86a88855b97cad7f849e5ebb9
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-14 13:40:42 +0300

    25-2, 25-3, 25-4

---
+++ 25-3.py
@@ -0,0 +1,16 @@
+a = [0]
+for n in range(592121, 593011+1):
+    deliteli=[]
+    sq = int(n**0.5)
+    for d in range(1, sq+1):
+        if d*d == sq:
+            deliteli.append(sq)
+        elif n % d == 0:
+            deliteli.append(d)
+            deliteli.append(n//d)
+    if len(deliteli) > a[0]:
+        a = [len(deliteli)]
+        a.append(deliteli[-1])
+        a.append(deliteli[-2])
+print(a)
+
