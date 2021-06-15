0dd7bb13ebb768056989a2bab1fda50ea83be8f6
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-16 00:31:23 +0300

    2.py tests

--- 2-1.py
+++ 2-1.py
@@ -3,6 +3,6 @@ for w in 0,1:
     for x in 0,1:
         for y in 0,1:
             for z in 0,1:
-                F = x
+                F = ((y <= z) or (not(x) and w)) == (w == z)
                 if F == True:
                     print(w,x,y,z)
--- 2-tests.py
+++ 2-tests.py
@@ -2,6 +2,6 @@ print('x y z')
 for x in 0,1:
     for y in 0,1:
         for z in 0,1:
-            F = (not(z) and x)
+            F = ((x or y) and not(z) and not(z == x))
             if F == True:
                 print(x,y,z)
