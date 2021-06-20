20252173510d3e7a8794d393dc87f51efaa22a44
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-20 05:12:56 +0300

    create 14.py and 16-2.py

--- 14.py
+++ 14.py
@@ -3,8 +3,7 @@ def decto(x, base):
     while x:
         s = str(x % base) + s
         x //= base 
-
     return s
 
-n = 9**5 + 3**25 - 20
-print(decto(n, 3))
+n = 4**590 + 8**350 - 2**1020 - 25
+print(decto(n, 2).count('0'))
