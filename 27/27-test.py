54aacc7352df57db67589e04c2c9fcfc73f82bd0
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-06-23 22:56:13 +0300

    27-test.py

--- 27/27-test.py
+++ 27/27-test.py
@@ -1,13 +1,12 @@
 f = open('27A-1-1.txt')
 n = int(f.readline())
 sum_f = 0
-min_razn = 9999999999999
+min_razn = 99999999
 for i in range(n):
     a, b = map(int, f.readline().split())
     sum_f += max(a, b)
-    if abs(a - b) < min_razn and abs(a - b) % 4 != 0:
+    if abs(a - b) > 0 and abs(a - b) % 10 != 0:
         min_razn = abs(a - b)
-if sum_f % 4 == 0:
+if sum_f % 10 == 0:
     print(sum_f - min_razn)
-else:
-    print(sum_f)
+print(sum_f)
