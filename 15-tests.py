570f8ac9c013f6252212fcf1327a851f4d0d2ea3
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-04-01 22:59:47 +0300

    working on ege

--- 15-tests.py
+++ 15-tests.py
@@ -1,21 +1,11 @@
-def deli(n, m):
-    if n % m == 0:
-        return True
-    else:
-        return False
-
-for A in range(1, 1000000000):
+result = -1
+for A in range(999999999):
     flag = True
-    for x in range(1, 100000):
-        F1 = not deli(x, A)
-        F2 = deli(x, 6)
-        F3 = not deli(x, 9)
-        F = F1 <= (F2 <= F3)
-
-        if F == 0:
-            flag = False
-            break
+    for x in range(999):
+        for y in range(999):
+            F = ((2 * x + y) != 70) or (x < y) or (A < x)
+            if F == 0:
+                flag = False
 
     if flag == True:
         print(A)
-
