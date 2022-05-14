21050554660bfdd7305fc988a92456462efa8483
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-05-15 00:50:36 +0300

    tests

--- 15-2.py
+++ 15-2.py
@@ -5,11 +5,11 @@ def deli(n, m):
         return False
 
 def F(A, x):
-    F1 = not deli(x, 18)
-    F2 = not deli(x, A)
-    F3 = not deli(x, 12)
-    return F1 <= (F2 <= F3)
+    F1 = deli(x, 3)
+    F2 = not deli(x, 5)
+    F3 = ((x + A) >= 70)
+    return (F1 <= F2) + F3
 
 for A in range(1, 1000):
-    if all(F(A, x) for x in range(1000)):
+    if all(F(A, x) for x in range(1, 1000)):
         print(A)
--- tests.py
+++ tests.py
@@ -1,20 +1,22 @@
-# На числовой прямой даны два отрезка: D = [17; 58] и C = [29; 80].
-# Укажите наименьшую возможную длину такого отрезка A, для которого логическое выражение
-# (x ∈ D) → ((¬(x ∈ C) /\ ¬(x ∈ A)) → ¬(x ∈ D)) истинно для всех x
+import functools
 
-for D in range(17, 58 + 1):
-    print("passing", D)
-    for C in range(29, 80 + 1):
-        for A in range(1, 100):
-            flag = True
-            for x in range(1, 100):
-                F1 = x in range(17, 58 + 1)
-                F2 = not(x in range(29, 80 + 1))
-                F3 = not(x in range(1, 100))
-                F4 = not(x in range(17, 58 + 1))
-                F = F1 <= ((F2 + F3) <= F4)
-                if F == False:
-                    flag = False
-            if flag == True:
-                print(A)
-                break
+def moves(heap):
+    return (heap + 1), (heap * 2)
+
+@functools.lru_cache(maxsize=None)
+
+def game(heap):
+    if heap >= 223:
+        return 'end'
+    elif any(game(x) == 'end' for x in moves(heap)):
+        return 'P1'
+    elif all(game(x) == 'P1' for x in moves(heap)):
+        return 'V1'
+    elif any(game(x) == 'V1' for x in moves(heap)):
+        return 'P2'
+    elif all(game(x) in {'P1', 'P2'} for x in moves(heap)):
+        return 'V2'
+
+for s in range(1, 210):
+    heap = s
+    print(s, game(heap))
