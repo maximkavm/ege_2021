395290af48b1fb041fe0cd0ee4fe323560b29f37
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-12-08 17:52:51 +0300

    Update 5-tests.py

--- 5-tests.py
+++ 5-tests.py
@@ -1,13 +1,10 @@
-def makeMagic(first, second):
-    senior = int(first[0]) + int(second[0])
-    middle = int(first[1]) + int(second[1])
-    junior = int(first[2]) + int(second[2])
-
-    array = [senior, middle, junior].sort(reverse=True)
-
-    s = str(array[0]) + str(array[1]) + str(array[2])
-    return s
-
-for i in range(100, 1000):
-    if makeMagic("486", str(i)) == str(13107):
-        print(i)
+for n in range(100):
+    r = bin(n)[2:]
+    if r.count('1') % 2 == 0:
+        r += '0'
+        r = '10' + r[2:]
+    else:
+        r += '1'
+        r = '11' + r[2:]
+    if int(r, 2) > 40:
+        print(n)
