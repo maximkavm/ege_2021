0f92f716a242bf21d1ef3273c1d06a8635bf4462
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-02-02 17:45:41 +0300

    tests

--- tests.py
+++ tests.py
@@ -1,47 +1,6 @@
-import sqlite3
+a = [[], [], [], [], [], [], [], [], [], []]
 
-global data, columns
-data = []
-columns = ('id', 'name', 'age', 'weight', 'sex', 'type', 'owner')
+for i in range(0, 2021):
+    a[(i*i*i) % 10].append(i)
 
-def get(filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.split(',')
-            data.append((int(line[0]), line[1], int(line[2]), float(line[3]), line[4], line[5], line[6].replace('\n', '')))
-    return data
-
-def put(filename):
-    with open(filename, 'w') as file:
-        for val in data:
-            file.write(','.join([str(i) for i in val]) + '\n')
-
-database = sqlite3.connect('pets.db')
-cursor = database.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS pets
-    (
-        id INT,
-        name TEXT,
-        age INT,
-        weight REAL,
-        sex TEXT,
-        type TEXT,
-        owner TEXT
-    )
-''')
-
-
-data = get('tests.txt')
-
-for n in data:
-    if n not in list(cursor.execute('SELECT * FROM pets')):
-        cursor.execute(f'INSERT INTO pets VALUES (?,?,?,?,?,?,?)', n)
-        database.commit()
-        print(f'Added {n}')
-    else:
-        print(f'User {n} is already in the database')
-
-for n in list(cursor.execute('SELECT * FROM pets')):
-    print(n)
+print(len(a[0]), len(a[1]),len(a[2]),len(a[3]),len(a[4]),len(a[5]),len(a[6]),len(a[7]),len(a[8]),len(a[9]))
