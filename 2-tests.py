7d7632117bd11d6b3f1c2ce5f7ed11f788f93dd7
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-26 20:35:13 +0300

    task 2

--- 2-tests.py
+++ 2-tests.py
@@ -1,9 +1,9 @@
-print("x y z w")
+print('x y z w')
 
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                F = ((x and y) or (y and z)) == ((not(x) or w) and (not(w) or z))
+for x in 0,1:
+    for y in 0,1:
+        for z in 0,1:
+            for w in 0,1:
+                F = ((x and not(y)) or (w <= z)) == (z == x)
                 if F == True:
-                    print(x, y, z, w)
+                    print(x, y, z, w o)
