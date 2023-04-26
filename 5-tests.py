c412d646236dc681dd092b2aa7caa634b5bac8cf
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-26 20:35:18 +0300

    task 5

--- 5-tests.py
+++ 5-tests.py
@@ -1,14 +1,12 @@
-for i in range(1000):
-    n = i
-    r = bin(n)[2:]
+count = 0
+for n in range(1000, 10000):
+    r = str(n)
+    if int(r[0]) % 4 == 0:
+        r = '9' + r[1:]
+    if int(r[0]) % 2 == 0 and int(r[0]) % 4 != 0:
+        r = '3' + r[1:]
 
-    if r.count('1') % 2 == 0:
-        r += '0'
-        r = '10' + r[2:]
-    else:
-        r += '1'
-        r = '11' + r[2:]
+    if r[0] == '9' and oct(int(r))[-1] == '4':
+        count += 1
 
-    if int(r, 2) > 40:
-        print(n)
-        break
+print(count)
